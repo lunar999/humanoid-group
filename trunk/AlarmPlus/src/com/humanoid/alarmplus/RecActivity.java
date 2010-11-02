bc7bcefe8e6e600db98ee9fd38edcc1a903c40cf
jaulim999 <jaulim999@56f69049-4d4c-c35b-687c-4afd535d3fc3>
2010-11-02 04:51:22 +0000

    녹음화면 수정 모델명으로 SD카드 인식부분 수정

--- trunk/AlarmPlus/src/com/humanoid/alarmplus/RecActivity.java
+++ trunk/AlarmPlus/src/com/humanoid/alarmplus/RecActivity.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import android.app.Activity;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -15,6 +16,7 @@ public class RecActivity extends Activity {
 	private Button btnRec;
 	private Button btnStop;
 	private MediaRecorder recorder;
+	private final String SAMSUNG_GALAXYS = "SHW-M110S";
 	
     /** Called when the activity is first created. */
     @Override
@@ -22,8 +24,23 @@ public class RecActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rec_layout);
         
+        //device id
+        
+//        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+//        String deviceId = telephonyManager.getDeviceId();
+//        
+//        Log.d("deviceId", deviceId);
+//        Log.d("getLine1Number", telephonyManager.getLine1Number()+"");
+//        Log.d("getDeviceSoftwareVersion", telephonyManager.getDeviceSoftwareVersion()+"");
+//        
+//        Log.d("Build.MODEL", Build.MODEL+"");
+        
+        
+        
+        
         btnRec = (Button) findViewById(R.id.btnRec);
     	btnStop = (Button) findViewById(R.id.btnStop);
+    	
     	
     	btnStop.setEnabled(false);
     	
@@ -56,10 +73,21 @@ public class RecActivity extends Activity {
     	
     	Log.d("TEST", "START_REC");
     	File recFile;
+    	String path = "";
     	try {
-			recFile = Environment.getExternalStorageDirectory();
-			Log.d("PATH", recFile.getAbsolutePath());
-			String path = recFile.getAbsolutePath() + "/sd/test_" + System.currentTimeMillis() + ".mp4";
+    		
+    		recFile = Environment.getExternalStorageDirectory();
+    		Log.d("PATH", recFile.getAbsolutePath());
+    		path = recFile.getAbsolutePath() + "/test_" + System.currentTimeMillis() + ".mp4";
+    		
+    		if(SAMSUNG_GALAXYS.equals(Build.MODEL)){
+    			File sd2 = new File(recFile.getAbsolutePath()+"/sd");
+    			if(sd2.exists()){
+    				path = recFile.getAbsolutePath() + "/sd/test_" + System.currentTimeMillis() + ".mp4";
+    			} else {
+    				path = recFile.getAbsolutePath() + "/test_" + System.currentTimeMillis() + ".mp4";
+    			}
+    		}
 			
 			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 			recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
