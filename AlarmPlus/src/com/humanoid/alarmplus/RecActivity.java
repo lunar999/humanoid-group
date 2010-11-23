40254cde40c57e84c3e680f1cc3000c1982de34f
jaulim999 <jaulim999@56f69049-4d4c-c35b-687c-4afd535d3fc3>
2010-11-23 03:27:22 +0000

    녹음화면 수정 및 유틸추가

--- AlarmPlus/src/com/humanoid/alarmplus/RecActivity.java
+++ AlarmPlus/src/com/humanoid/alarmplus/RecActivity.java
@@ -29,94 +29,81 @@ import com.humanoid.alarmplus.Alarms;
 
 public class RecActivity extends Activity implements AlarmConstantIf{
 
-//	private static final String ALARM_REC_POSTFIX = ".mp4";
+	//	private static final String ALARM_REC_POSTFIX = ".mp4";
 	private static final String SD2 = "sd";
 	private static final String ALARM_REC_PREFIX = "alarm_rec.mp4";
 	private static final String SAMSUNG_GALAXYS = "SHW-M110S";
-    private static final float IN_CALL_VOLUME = 0.125f;
-    
-    
+	private static final float IN_CALL_VOLUME = 0.125f;
+
+	private String path = "";
 	private static final boolean DEBUG_MODE = true;
-	
+
 	private Button btnRec;
 	private Button btnStop;
-	
 	private Button btnListen;
 	private Button btnClosed;
-	
 	private MediaRecorder recorder;
-	
 	private Bitmap micImage_green;
 	private Bitmap micImage_red;
-	
 	private ImageView imageView;
-	
 	private MediaPlayer mMediaPlayer;
-	
-	Chronometer chronometer;
+	private Chronometer chronometer;
+
+	View.OnClickListener mResetListener = new OnClickListener() {
+		public void onClick(View v) {
+			chronometer.setBase(SystemClock.elapsedRealtime());
+		}
+	};
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.rec_layout);
+		
+		UtilFile.makeAlarmDir();
+
 		micImage_green = BitmapFactory.decodeResource(getResources(), R.drawable.mic_128);
 		micImage_red = BitmapFactory.decodeResource(getResources(), R.drawable.mic_red_128);
 
 		btnRec = (Button) findViewById(R.id.btnRec);
 		btnStop = (Button) findViewById(R.id.btnStop);
-		
 		btnListen = (Button) findViewById(R.id.btnListen);
 		btnClosed = (Button) findViewById(R.id.btnClosed);
-		
 		imageView = (ImageView)findViewById(R.id.mic_128);
 		chronometer = (Chronometer) findViewById(R.id.chronometer);
-		
-		btnStop.setEnabled(false);
 
-		mMediaPlayer = new MediaPlayer();
-		mMediaPlayer.setOnErrorListener(new OnErrorListener() {
-	         public boolean onError(MediaPlayer mp, int what, int extra) {
-	             Log.d("","Error occurred while playing audio.");
-	             mp.stop();
-	             mp.release();
-	             mMediaPlayer = null;
-	             return true;
-	         }
-	     });
-		
-		mMediaPlayer.setVolume(IN_CALL_VOLUME, IN_CALL_VOLUME);
-//		mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(),afd.getLength());
-		try {
-			mMediaPlayer.setDataSource("/sdcard/humanoid/alarm/alarm_rec.mp4");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		setBtnListener();
+	}
+
+	/**
+	 * 버튼 리스너 적용
+	 */
+	private void setBtnListener() {
+		btnStop.setEnabled(false);
 		btnRec.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				btnRec.setEnabled(false);
 				runOnUiThread(new Runnable() {
-					
+
 					@Override
 					public void run() {
 						chronometer.setBase(SystemClock.elapsedRealtime());
 						chronometer.start();
 					}
 				});
-				
+
 				runOnUiThread(new Runnable() {
-					
+
 					@Override
 					public void run() {
 						startRec();
-						
 					}
 				});
-				
+
 				btnStop.setEnabled(true);
-				
+
 				imageChange(true);
 
 			}
@@ -129,23 +116,23 @@ public class RecActivity extends Activity implements AlarmConstantIf{
 				chronometer.stop();
 				stopRec();
 				btnRec.setEnabled(true);
-				
+
 				imageChange(false);
 			}
 		});
-		
+
 		btnListen.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				//play
-//				try {
-//					startAlarm();
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
+				try {
+					startAlarm();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
-		
+
 		btnClosed.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
@@ -153,24 +140,61 @@ public class RecActivity extends Activity implements AlarmConstantIf{
 			}
 		});
 	}
-	
-	
-	private void startAlarm()
-	throws java.io.IOException, IllegalArgumentException,
-	IllegalStateException {
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		
+
+		mMediaPlayer = new MediaPlayer();
+		mMediaPlayer.setOnErrorListener(new OnErrorListener() {
+			public boolean onError(MediaPlayer mp, int what, int extra) {
+				Log.d("","Error occurred while playing audio.");
+				mp.stop();
+				mp.release();
+				mMediaPlayer = null;
+				return true;
+			}
+		});
+
+		mMediaPlayer.setVolume(IN_CALL_VOLUME, IN_CALL_VOLUME);
+		//			mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(),afd.getLength());
+
+
+	}
+
+
+	/**
+	 * 
+	 * @throws java.io.IOException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalStateException
+	 */
+	private void startAlarm() throws java.io.IOException, IllegalArgumentException,	IllegalStateException {
 		final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
 		// do not play alarms if stream volume is 0
 		// (typically because ringer mode is silent).
 		if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
-			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
-			mMediaPlayer.setLooping(false);
-			mMediaPlayer.prepare();
-			mMediaPlayer.start();
-			
-			mMediaPlayer.stop();
-            mMediaPlayer.release();
+			try {
+				mMediaPlayer.setDataSource("/sdcard/humanoid/alarm/alarm_rec.mp4");
+				mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+				mMediaPlayer.setLooping(false);
+				mMediaPlayer.prepare();
+				mMediaPlayer.start();
+
+				mMediaPlayer.stop();
+				mMediaPlayer.release();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
+
+	/**
+	 * 
+	 * @param isStart
+	 */
 	private void imageChange(boolean isStart) {
 		if(isStart) {
 			imageView.setImageBitmap(micImage_red);
@@ -179,43 +203,29 @@ public class RecActivity extends Activity implements AlarmConstantIf{
 			imageView.setImageBitmap(micImage_green);
 		}
 	}
-	
-	View.OnClickListener mResetListener = new OnClickListener() {
-        public void onClick(View v) {
-            chronometer.setBase(SystemClock.elapsedRealtime());
-        }
-    };
 
-
+	/**
+	 * 녹음시작
+	 */
 	private void startRec(){
 		if(recorder == null){
 			recorder = new MediaRecorder();
 		}
-		
+
 		if(DEBUG_MODE){
 			Log.d("TEST", "START_REC");
 			Log.d("Build.MODEL", Build.MODEL);
 			Log.d("getExternalStorageState", android.os.Environment.getExternalStorageState()+"");
 		}
-		
-		File recFile = Environment.getExternalStorageDirectory();
-		String path = "";
-		try {
-			
-			String state = android.os.Environment.getExternalStorageState();
-			if(!state.equals(android.os.Environment.MEDIA_MOUNTED))  {
-				//path = getFilesDir().getAbsolutePath() + File.separator + ALARM_REC_PREFIX ;
-				path = getFilesDir().getAbsolutePath() + File.separator + UtilFile.recpath ;	 // 10.11.22 update redmars				
-			} else {//sdcard
-				
-				//path = UtilFile.getSdCardAlarmPath(ALARM_REC_PREFIX);
-				path = UtilFile.getSdCardAlarmPath(UtilFile.recpath);		 // 10.11.22 update redmars
 
-			}
+//		File recFile = Environment.getExternalStorageDirectory();
+
+		try {
 			if(DEBUG_MODE){
 				Log.d("PATH", path);
 			}
-			
+			path = getFilePath();
+
 			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 			recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
 			recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
@@ -233,11 +243,29 @@ public class RecActivity extends Activity implements AlarmConstantIf{
 			recorder.stop();
 			recorder.release();
 			recorder = null;
-			
+
 			if(DEBUG_MODE){
 				Log.d("TEST", "STOP_REC");
 			}
 		}
+	}
+
+	/**
+	 * 녹음파일 저장장소를 리턴함
+	 * @author lunar999
+	 */
+	private String getFilePath(){
+		String filePath = "";
+		String state = android.os.Environment.getExternalStorageState();
+		if(!state.equals(android.os.Environment.MEDIA_MOUNTED))  {
+			filePath = getFilesDir().getAbsolutePath() + File.separator + UtilFile.recpath ;				
+		} else {
+			//sdcard
+			filePath = UtilFile.getSdCardAlarmPath(UtilFile.recpath);
+
+		}
+
+		return filePath;
 	}
 
 
